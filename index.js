6de1e88e583f56b9e7ce8862aff9dff2c3cb0d0f
asim <152641117+Masim6604@users.noreply.github.com>
2025-07-08 16:53:56 +0500

    first

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/home', (req, res) => {
+   res.send('Hello from Jenkins and Docker!');
+});
+
+app.listen(8000, () => {
+    console.log("Server is listening on port 8000");
+});
